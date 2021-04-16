c55db9c3e9b1f949057f4d9968cf9d4af71381d4
Kelvin Mwenda <kelvinmwenda9@gmail.com>
2021-04-16 14:21:51 +0300

    Initial commit for kotlin fundamentals

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("This is awesome, finally coding on kotlin")
+
+    //val name = "kelvin"  // val cannot be reassigned in kotlin
+    var name = "ann"  // var can be reassigned
+
+    val age: Int = 40
+    println("I am ${age} years old")
+
+    name = "mike"
+
+    println("This is my name ${name}")
+    println(name.isEmpty())
+}
